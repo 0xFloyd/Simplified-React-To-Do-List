3f780a2978d0ca1037aa0e0e59de587189046dcf
Ryan Floyd <arfloyd7@gmail.com>
2019-10-08 20:41:47 -0400

    Updates

---
+++ precache-manifest.72f66d888facd20f2f0e1ea96faec3f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60c9253007edeb1000749a745844f689",
+    "url": "/Simplified-React-To-Do-List/index.html"
+  },
+  {
+    "revision": "800578d200884a174fe3",
+    "url": "/Simplified-React-To-Do-List/static/css/main.498cf7df.chunk.css"
+  },
+  {
+    "revision": "6bfe832d682f46cabc1a",
+    "url": "/Simplified-React-To-Do-List/static/js/2.71c02b92.chunk.js"
+  },
+  {
+    "revision": "800578d200884a174fe3",
+    "url": "/Simplified-React-To-Do-List/static/js/main.e4c12932.chunk.js"
+  },
+  {
+    "revision": "a6b0c135cacd734df437",
+    "url": "/Simplified-React-To-Do-List/static/js/runtime-main.a22c30dd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Simplified-React-To-Do-List/index.html/precache-manifest.de702ea14efd386203d0e1538c66d22c.js"
+  "/Simplified-React-To-Do-List/precache-manifest.72f66d888facd20f2f0e1ea96faec3f5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Simplified-React-To-Do-List/index.html/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Simplified-React-To-Do-List/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
