62fb490278d9ccfccc3cf049b29f6c5ab04be25d
Ryan Floyd <arfloyd7@gmail.com>
2019-10-08 20:34:49 -0400

    Updates

---
+++ precache-manifest.b9cc6a6601b70b9151eb44661c5d039c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b61c55fd63d30e2a8579298497b3217",
+    "url": "/Simplified-React-To-Do-List/index.html"
+  },
+  {
+    "revision": "5c80a40d45486febbfa5",
+    "url": "/Simplified-React-To-Do-List/static/css/main.5262627b.chunk.css"
+  },
+  {
+    "revision": "6bfe832d682f46cabc1a",
+    "url": "/Simplified-React-To-Do-List/static/js/2.71c02b92.chunk.js"
+  },
+  {
+    "revision": "5c80a40d45486febbfa5",
+    "url": "/Simplified-React-To-Do-List/static/js/main.e4c12932.chunk.js"
+  },
+  {
+    "revision": "a6b0c135cacd734df437",
+    "url": "/Simplified-React-To-Do-List/static/js/runtime-main.a22c30dd.js"
+  }
+]);
